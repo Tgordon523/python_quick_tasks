613d1daf294c376e9becb0b85bb939f254cc05f2
unknown <tgordon523@gmail.com>
2023-09-17 22:03:39 -0500

    black formatted scripts

--- sum_two.py
+++ sum_two.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 ### Add Two
-def sum_two_index(nums:list, result:int):
+def sum_two_index(nums: list, result: int):
     ### Return two indexes for the proper sums
     prevMat = {}
 
     for i, v in enumerate(nums):
         # print(v)
-        diff = result - v 
+        diff = result - v
         if diff in prevMat:
             return [prevMat[diff], i]
         prevMat[v] = i
-    return      
+    return
